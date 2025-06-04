93ae2c9b52937cf8677da6afaa32d9852572d812
Zack Sigmund <zsigmund@overdrive.com>
2025-06-04 15:19:41 -0400

    Ex5 bug: pluralization

--- src/components/WinBanner/WinBanner.js
+++ src/components/WinBanner/WinBanner.js
@@ -5,7 +5,7 @@ function WinBanner({ numGuesses }) {
     <div className="happy banner">
       <p>
         <strong>Congratulations!</strong> Got it in{" "}
-        <strong>{numGuesses} guesses</strong>.
+        <strong>{numGuesses} {numGuesses === 1 ? "guess" : "guesses"}</strong>.
       </p>
     </div>
   );
